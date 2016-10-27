fc6aa493ce303d3b7047bc08600db5961862d96f
Remco Visser <rvremco@hotmail.com>
2016-10-27 11:39:42 +0200

    removed old code

--- Controllers/UserController.js
+++ Controllers/UserController.js
@@ -6,19 +6,7 @@ module.exports = {
 
   index: function(req, res, next) {
     var data = userModel.index();
-    
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost');
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
+  
     res.send(data);
   },
 
