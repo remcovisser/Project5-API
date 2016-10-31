c4469ebedadb9e26f9b4715a48524e7f3b0bca43
Remco Visser <rvremco@hotmail.com>
2016-10-31 12:02:36 +0100

    fixes in userModel

--- Models/UserModel.js
+++ Models/UserModel.js
@@ -2,27 +2,27 @@ var baseModel = require(__base + '/Models/BaseModel');
 
 
 exports.index = function(res) {
-    baseModel.get(res, 'SELECT * from User');
+    baseModel.get(res, "SELECT * from User");
 }
 
 exports.show = function(res, id)
 {
-    baseModel.get(res, 'SELECT * from User WHERE user_id = ' + id);
+    baseModel.get(res, "SELECT * from User WHERE user_id = " + id);
 }
 
 exports.store = function(res, params) 
 {
-    baseModel.send(res, 'INSERT INTO User(`username`, `password`, `email`, `date_of_birth`, `admin`) VALUES ("'+params.username+'", "'+params.password+'", "'+params.email+'", "'+params.date+'", 0)');     
+    baseModel.send(res, "INSERT INTO User(username, password, email, date_of_birth, admin) VALUES ('"+params.username+"', '"+params.password+"', '"+params.email+"', '"+params.date_of_birth+"', "+params.admin+")");    
 }
 
 exports.update = function(res, params) 
 {
-    baseModel.send(res, 'UPDATE User SET `username` = "'+params.username+'", `password` = "'+params.password+'", `email` = "'+params.email+'" WHERE `user_id` = '+params.user_id+'');     
+    baseModel.send(res, "UPDATE User SET username = '"+params.username+"', password = '"+params.password+"', email = '"+params.email+"', date_of_birth = '"+params.date_of_birth+"', admin =  "+params.admin+" WHERE user_id = '+params.user_id+'");     
 }
 
 exports.destroy = function(res, id) 
 {
-    baseModel.send(res, 'UPDATE User SET `boolean_deleted` = 1 WHERE `user_id` = ' + id);     
+    baseModel.send(res, "UPDATE User SET `boolean_deleted` = 1 WHERE `user_id` = " + id);     
 }
 
 
