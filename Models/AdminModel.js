fe62719f32347dd00378bf5709f2f2bf1a54525c
Remco Visser <rvremco@hotmail.com>
2016-11-11 15:58:57 +0100

    idk

--- Models/AdminModel.js
+++ Models/AdminModel.js
@@ -20,7 +20,7 @@ module.exports = {
             res.send(parsedData);
         };
 
-        baseModel.getData(res, "SELECT x.create_date, COUNT(DISTINCT y.user_id) as users, DAY(x.create_date) as day FROM User x JOIN User y ON y.create_date <= x.create_date GROUP BY create_date", callback);
+        baseModel.getData(res, "SELECT COUNT(DISTINCT y.user_id) as users, DAY(x.create_date) as day FROM User x JOIN User y ON y.create_date <= x.create_date GROUP BY day", callback);
     },
 
     bestSellingProducts: function(res, amount)
