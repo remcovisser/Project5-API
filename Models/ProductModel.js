b9d9daf7c2afb57f5ce93a755bbd77763488bd8a
Remco Visser <rvremco@hotmail.com>
2017-01-19 18:03:20 +0100

    flush cache when corresponding data is modified

--- Models/ProductModel.js
+++ Models/ProductModel.js
@@ -3,10 +3,10 @@ var baseModel = require(__base + '/Models/BaseModel');
 module.exports = {
     index: function(res)
     {
-        __cache.get( "product-index", function( err, value ){
+        __cache.get( "products", function( err, value ){
             if(!err && value == undefined){
                 var callback = function(data) {
-                    __cache.set("product-index", data, 3600);
+                    __cache.set("products", data, 3600);
                     res.send(data);
                 }
                 baseModel.getData(res, "SELECT * from Product WHERE boolean_deleted = 0", callback);
@@ -24,15 +24,18 @@ module.exports = {
     store: function(res, params)
     {
         baseModel.send(res, "INSERT INTO Product SET ?", params);
+        __cache.del("products");
     },
 
     update: function(res, params, id)
     {
         baseModel.send(res, "UPDATE Product SET ? WHERE product_id = " + baseModel.mysql.escape(id), params);
+        __cache.del("products");
     },
 
     destroy: function(res, id)
     {
         baseModel.send(res, "UPDATE Product SET boolean_deleted = 1 WHERE product_id = " + baseModel.mysql.escape(id));
+        __cache.del("products");
     }
 };
