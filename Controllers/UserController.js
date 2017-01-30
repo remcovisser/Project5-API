33d61dd7c398430991240b172b35fa189dd2dee0
Jason Sjambar <realjase@gmail.com>
2017-01-30 13:27:13 +0100

    reverted the creation of an user

--- Controllers/UserController.js
+++ Controllers/UserController.js
@@ -57,8 +57,18 @@ module.exports = {
 
   store: function(req, res, next) {
       var data = JSON.parse(req.body);
-      userModel.store(res, data);
+      __validate.uniqueUsername(data.username, callback)
+
+      function callback(result) {
+        
+        if(result) {
+            userModel.store(res, data);
+        } else {
+            res.send(400, "Error, This username has already been taken.");
+        }
+
       next();
+      }
   },
 
   update: function(req, res, next) {
